14752992f5736bd154e0e07eae6b87a46f2461f1
keisato848 <habnk1227@gmail.com>
2026-02-11 10:35:34 +0900

    docs: AppInsights テレメトリ未送信の調査報告書を追加 + エージェント指示書改善 + 一時ファイルのgit追跡解除

--- debug-exams.js
+++
@@ -1,7 +0,0 @@
-fetch('http://localhost:3001/api/exams')
-    .then(res => res.json())
-    .then(data => {
-        console.log('Count:', data.length);
-        data.forEach(e => console.log(`ID: ${e.id} | CAT: ${e.category} | TITLE: ${e.title}`));
-    })
-    .catch(e => console.error(e));
